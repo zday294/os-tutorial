b7a91e8511fc650ebeb52a18ba52b1ff9995f6e6
zday294 <zday294@students.bju.edu>
2021-08-04 22:05:40 -0400

    attempt at loop boot record

---
+++ 02-bootsector-print/boot_sect_hello_loop.asm
@@ -0,0 +1,20 @@
+.data
+    message db 72 101 108 108 111 32 119 111 114 108 100 33 13 10 ; "Hello world!"
+
+
+.code
+    mov ah, 0x0e ; tty mode
+    mov cx, 14 ; length of the message array
+begin:
+    lea bx, [message] ; load the first address of message array
+hello:
+    mov al, [bx]
+    int 0x10
+    inc bx
+    cmp bx, cx
+    jl  hello
+    jmp begin ; jump to current address = infinite loop
+
+    ; padding and magic number
+    times 510 - ($-$$) db 0
+    dw 0xaa55 
