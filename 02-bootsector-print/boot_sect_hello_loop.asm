0b0d05cfc5dc27405cd2d2fc193d6d81664b97e3
zday294 <zday294@students.bju.edu>
2021-08-04 22:14:02 -0400

    possible bug fix

--- 02-bootsector-print/boot_sect_hello_loop.asm
+++ 02-bootsector-print/boot_sect_hello_loop.asm
@@ -1,13 +1,13 @@
-.data
-    message db 72 101 108 108 111 32 119 111 114 108 100 33 13 10 ; "Hello world!"
+section .data
+    message db 72 101 108 108 111 32 119 111 114 108 100 33 13 10 0; "Hello world!"
 
 
-.code
+section .code
     mov ah, 0x0e ; tty mode
     mov cx, 14 ; length of the message array
-begin:
+.begin:
     lea bx, [message] ; load the first address of message array
-hello:
+.hello:
     mov al, [bx]
     int 0x10
     inc bx
